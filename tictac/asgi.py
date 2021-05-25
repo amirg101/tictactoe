300f9e72319837a171e75907b323d31e1c3081da
amirg101 <amirgazi101@gmail.com>
2021-05-25 13:10:12 +0530

    first commit

---
+++ tictac/asgi.py
@@ -0,0 +1,24 @@
+
+import os
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from home.consumers import GameRoom
+from django.core.asgi import get_asgi_application
+from django.urls import path
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tictac.settings')
+
+application = get_asgi_application()
+
+
+ws_pattern = [
+        path('ws/game/<room_code>' , GameRoom)
+]
+
+
+application= ProtocolTypeRouter(
+    {
+        'websocket':AuthMiddlewareStack(URLRouter(
+            ws_pattern
+        ))
+    }
+)
